17c4856cdae9171356a29834a9768a233f3e7236
Cayo Bayestorff <bayesrox@gmail.com>
2020-10-22 18:41:58 +1100

    allowing port to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 // module.exports = index;
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
